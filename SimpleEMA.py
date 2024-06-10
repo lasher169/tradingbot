dafcbb98244e4f32a0e539b9de8e91d490181ea3
christopherau <chrisau168@gmail.com>
2024-06-10 10:29:47 +1000

    handle exceptions

--- SimpleEMA.py
+++ SimpleEMA.py
@@ -10,6 +10,12 @@ from contextlib import redirect_stdout
 tickers = []  # Add your list of tickers
 
 
+# Function to detect EMA crossovers
+def detect_crossovers(short_ema, long_ema):
+    crossover_up = (short_ema > long_ema) & (short_ema.shift(1) <= long_ema.shift(1))
+    crossover_down = (short_ema < long_ema) & (short_ema.shift(1) >= long_ema.shift(1))
+    return crossover_up, crossover_down
+
 def process_tickers(data):
     tickers = []
     rows =  data.get('data').get('table').get('rows', {})
@@ -49,28 +55,82 @@ if nasdaq_data:
     print(nasdaq_data)
 
 def get_stock_data(ticker):
+
     current_date = datetime.today()
     df = yf.download(ticker, period="3mo", interval="1d", start = current_date - relativedelta(months=4), end=current_date)
-
+    if df.size == 0:
+        return None
     return df
 
-def calculate_ema(df, length=30):
-    df["EMA"] = ta.ema(df["Close"], length=length)
-    return df
+def golden_cross(short_ema, long_ema, period=5):
+    """
+    Determine if a Golden Cross (short EMA crosses above long EMA) occurs.
 
-def is_upward_ema(df, length=30):
-    ema = df["EMA"].tail(length)
-    return all(ema.iloc[i] < ema.iloc[i + 1] for i in range(len(ema) - 1))
+    Parameters:
+        short_ema (pd.Series): Short-term Exponential Moving Average (EMA).
+        long_ema (pd.Series): Long-term Exponential Moving Average (EMA).
+
+    Returns:
+        bool: True if a Golden Cross occurs, False otherwise.
+    """
+    try:
+        crossover_condition = (short_ema > long_ema) & (short_ema.shift(1) <= long_ema.shift(1))
+        return any(crossover_condition.rolling(window=period).sum() > 0)
+    except:
+        return None
+
 
 upward_trending_stocks = []
+downward_trending_stocks = []
+
 def calc_ema(ticker):
     print("working on", ticker)
     df = get_stock_data(ticker)
-    df = calculate_ema(df)
-    if is_upward_ema(df):
-        print("adding ",ticker)
-        upward_trending_stocks.append(ticker)
-    print("completed", ticker)
+
+    if df is not None:
+        df['EMA_5'] = ta.ema(df["Close"], length=5)
+        df['EMA_30'] = ta.ema(df["Close"], length=30)
+
+        # Calculate On-Balance Volume (OBV)
+        df['OBV'] = ta.obv(df['Close'], df['Volume'])
+
+        # if is_upward_ema(df):
+        #     print("adding ",ticker)
+        #     upward_trending_stocks.append(ticker)
+
+        trend_result = golden_cross(df['EMA_5'], df['EMA_30'])
+        if  trend_result is not None and trend_result:
+            print("adding ",ticker)
+            upward_trending_stocks.append(ticker)
+            crossover_up, crossover_down = detect_crossovers(df['EMA_5'], df['EMA_30'])
+
+            # Plotting the results (optional, requires matplotlib)
+            import matplotlib.pyplot as plt
+
+
+            plt.figure(figsize=(28, 7))
+            # plt.plot(df['Close'], label='Close Price', color='black')
+            plt.plot(df['EMA_5'], label='5-day EMA', color='#D1FFBD')
+            plt.plot(df['EMA_30'], label='30-day EMA', color='red')
+            plt.xlabel('Date')
+            plt.ylabel('Price')
+
+            plt.twinx()
+            plt.plot(df['OBV'], label='OBV', color='aqua', linestyle='--')
+
+            plt.scatter(df.index[crossover_up], df['Close'][crossover_up], marker='^', color='green', label='Golden Cross',
+                        s=100)
+            plt.scatter(df.index[crossover_down], df['Close'][crossover_down], marker='v', color='red', label='Death Cross',
+                        s=100)
+            plt.legend()
+            plt.title('EMA Crossover Strategy with OBV '+ticker)
+            print("plotting ",ticker)
+            plt.show(block=False)
+            plt.show()
+
+        print("completed", ticker)
+    else:
+        print(ticker.upper(), "failed")
 
 # Example usage
 
@@ -79,8 +139,9 @@ data = fetch_nasdaq_stocks()
 tickers = process_tickers(data)
 
 for ticker in tickers:
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=250) as executor:
         future = executor.submit(calc_ema(ticker))
+    # calc_ema("dhcnl")
 
 
 
